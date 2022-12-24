7d236de6ba333f70d8527fb408387d22bfbe814a
Rostyslav Onikiienko <r.onikienko@gmail.com>
2022-12-24 12:14:29 +0200

    fix: update video schema (remove require true)

--- src/models/video.js
+++ src/models/video.js
@@ -2,37 +2,29 @@ const {Schema, model} = require('mongoose');
 
 const VideoSchema = new Schema({
     isRu: {
-        required: true,
         type: Number,
     },
     uid: {
-        required: true,
         type: String,
     },
     channelName: {
-        required: false,
         type: String,
         default: null,
     },
     link: {
-        required: true,
         type: String,
     },
     reason: {
-        required: true,
         type: String,
     },
     reasonDetails: {
-        required: false,
         type: String,
         default: null,
     },
     timeWhenBlocked: {
-        required: true,
         type: Number,
     },
     title: {
-        required: true,
         type: String,
     },
 });
